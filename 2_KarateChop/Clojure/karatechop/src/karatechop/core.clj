c04658b3352dff84c3d03e7529e9a7c503f40784
Chris Ortman <chrisortman@gmail.com>
2012-09-22 00:11:47 -0500

    I'm closer, only 2 failures!!
    The Clojure Programming book is really helpful

--- 2_KarateChop/Clojure/karatechop/src/karatechop/core.clj
+++ 2_KarateChop/Clojure/karatechop/src/karatechop/core.clj
@@ -1,53 +1,43 @@
 (ns karatechop.core
   (:gen-class))
 
-;Why the heck is (< 3 (count [])) not what i think?
+(defn middle [arr]
+	(let [c (count arr) ]
+		 (cond
+		 	(= 0 c) -1
+		 	(= 1 c) -1
+		 	(= 3 c) 1
+		 	(even? c) (/ c 2)
+		 	(odd? c) (/ (- c 1)))))
 
-(defn simple [arr]
-	(let [c (count arr)]
-		(or (= 0 c)
-			(< c 3))))
-
-(defn find-position [search start array]
-	(if (= (get array start) search)
-		0
-		(if (< (get array start) search)
-			-1
-			(if (> (get array start) search )
-				1))))
-
-(defn find-middle [arr]
-	(let [c (count arr)]
-		(if (even? c)
-			(/ c 2)
-			(/ (- c 1) 2))))
-
-
-(defn not-found []
-	-1)
-
-(defn find-in-simple [search array_to_search]
-	(let [c (count array_to_search)]
-		(= 0 c)
-			(not-found)
-			(if (and (= 1 c)
-				(= search (get array_to_search 0)))
-				0
-				(if (and (= 2 c)
-					(= search (get array_to_search 1)))
-					1
-					(not-found)))))
-
-(defn check-edges [search_target array_to_search lower_bound upper_bound]
-	(if (= search_target (get lower_bound array_to_search))
-		lower_bound
-		(if (= search_target (get upper_bound array_to_search))
-			upper_bound
-			-1)))
+(defn binary-split [search_target array_to_search mid-point]
+	(let [middle-value (nth array_to_search mid-point)]
+		 (cond
+		 	(<  middle-value search_target) [(subvec array_to_search mid-point (count array_to_search)) mid-point]
+		 	(>= middle-value search_target) [(subvec array_to_search 0 mid-point) 0]
+		 	:else [])))
 
 (defn chop [search_target array_to_search]
 	"I slice and i dice yo"
-	0)
+	(println "Searching" array_to_search)
+	(if (= 0 (count array_to_search))
+		-1
+		(let [
+			  left-edge  (first array_to_search)
+			  right-edge (last array_to_search)
+			  mid-point (middle array_to_search)
+			 ]
+			 (cond 
+			 	(= left-edge search_target) 0
+			 	(= right-edge search_target) (- (count array_to_search) 1)
+			 	(not= mid-point -1) (let [
+			 								[half-array pad] (binary-split search_target array_to_search mid-point)
+			 							 ]
+			 							 (let [inner-result (chop search_target half-array)]
+			 							 	(if (not= -1 inner-result)
+			 							 		(+ pad inner-result)
+			 							 		-1)))
+			 	:else -1))))
 
 	
 (defn -main
